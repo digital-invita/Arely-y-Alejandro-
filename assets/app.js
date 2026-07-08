c8dd51c887425792b6fcdf8aab8143612a591523
digital-invita <anuar.rios.310@gmail.com>
2026-07-07 22:07:18 -0600

    Fix RSVP phone number formatting
    Removed spaces from the RSVP phone number format.

--- assets/app.js
+++ assets/app.js
@@ -28,7 +28,7 @@
     "eventEnd": "2026-07-25T11:59",
     "timeZone": "America/Mexico_City",
     "eventLocation": "Lugar de la recepción",
-    "rsvpPhone": "‪844 196 8271‬"
+    "rsvpPhone": "‪8441968271‬"
   },
   "theme": {
     "bgTop": "#fffaf2",
